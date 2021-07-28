3ef1ee970a0f39bd95a1e3dfc86a4659bfd58021
sistemasinfinitos <Infinito@SERVIDOR>
2021-07-28 06:09:29 -0500

    Victoriana

---
+++ PatronesGof/DeCreacion/AbstractFactory/ISofa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatronesGof.DeCreacion.AbstractFactory
+{
+    public interface ISofa
+    {
+    }
+}
